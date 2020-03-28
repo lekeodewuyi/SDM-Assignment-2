98b6a6c78344e322a6d790c65a67e6e0b1830205
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-03-28 13:47:39 -0400

    okapi BM25L

--- okapi.js
+++ okapi.js
@@ -16,3 +16,28 @@ for(all) {
     ((k1 + 1) * tf[i])/(k1 * ((1 - b) + (b * docLength/docLengthAvg)) + tf[i]) *
     ((k3 + 1) * qf[i])/(k3 + qf[i])
 }
+
+// Okapi BM25
+
+for(all) {
+    score +=
+    log((docN - DF[i] + 0.5)/(DF[i] + 0.5)) *
+    ((k1 + 1) * tf[i])/(k1 * ((1 - b) + (b * docLength/docLengthAvg)) + tf[i]);
+}
+
+
+// Okapi BM25+
+
+for(all) {
+    score +=
+    log((docN - DF[i] + 0.5)/(DF[i] + 0.5)) *
+    (((k1 + 1) * tf[i])/(k1 * ((1 - b) + (b * docLength/docLengthAvg)) + tf[i]) + d);
+}
+
+// Okapi BM25L
+
+for(all) {
+    score +=
+    log((docN + 1)/(DF[i] + 0.5)) *
+    ((k1 + 1) * tf[i])/(k1 * ((1 - b) + (b * docLength/docLengthAvg)) + tf[i]);
+}
