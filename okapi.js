b3c2dd1d2275fe9afe1d5ec54c453605a7ae0c01
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-03-28 11:56:50 -0400

    original okapi weighting

--- okapi.js
+++ okapi.js
@@ -1,5 +1,8 @@
 // Original Okapi
 
 for(all) {
-    
+    score +=
+    log((docN - DF[i] + 0.5)/(DF[i] + 0.5)) *
+    ((k1 + 1) * tf[i])/(k1 * ((1 - b) + (b * docLength/docLengthAvg)) + tf[i]) *
+    ((k3 + 1) * qf[i])/(k3 + qf[i])
 }
