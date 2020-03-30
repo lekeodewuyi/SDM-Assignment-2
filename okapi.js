9f6f561748a76e4447d97a2f5658ef6ba24c2adc
Oluwaleke Odewuyi <lekeodewuyi@gmail.com>
2020-03-29 20:03:21 -0400

    original okapi 2

--- okapi.js
+++ okapi.js
@@ -11,6 +11,14 @@ for(all) {
     ((k3 + 1) * qf[i])/(k3 + qf[i])
 }
 
+// original Okapi 2
+
+for(all) {
+    score+=
+    log((docN - DF[i] + 0.5)/(DF[i] + 0.5)) *
+    ((k1 + 1) * tf[i])/(k1 * ((1 - b) + b * doclength/docLengthAvg) + tf[i]) *
+    ((k3 + 1) * qf[i])/(k3 + qf[i])
+}
 
 // Modified Okapi UIUC
 
